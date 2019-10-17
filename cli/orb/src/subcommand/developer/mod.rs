91d726e86110e9c934159970c789cfbbd22a43ba
T.J. Telan <t.telan@gmail.com>
2019-10-17 01:12:12 -0700

    Adding in dev commands using docker apis. Pulling images, and a stub for creating containers.

--- cli/orb/src/subcommand/developer/mod.rs
+++ cli/orb/src/subcommand/developer/mod.rs
@@ -3,11 +3,13 @@ use structopt::StructOpt;
 use crate::{GlobalOption, SubcommandError};
 
 pub mod git;
+pub mod docker;
 
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab_case")]
 pub enum DeveloperType {
     Git(git::SubcommandOption),
+    Docker(docker::SubcommandOption),
 }
 
 pub fn subcommand_handler(
@@ -16,5 +18,6 @@ pub fn subcommand_handler(
 ) -> Result<(), SubcommandError> {
     match dev_subcommand {
         DeveloperType::Git(sub_option) => git::subcommand_handler(global_option, sub_option),
+        DeveloperType::Docker(sub_option) => docker::subcommand_handler(global_option, sub_option),
     }
 }
