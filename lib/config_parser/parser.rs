4055608f4de8dcd8c277a7ce0a6b1bd80a04772f
T.J. Telan <tjtelan@level11.com>
2019-10-24 18:14:02 -0700

    Making fields in OrbitalConfig pub, and cleaning up debug output

--- lib/config_parser/parser.rs
+++ lib/config_parser/parser.rs
@@ -2,15 +2,16 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct OrbitalConfig {
-    image: String,
-    command: Vec<String>,
+    pub image: String,
+    pub command: Vec<String>,
 }
 
 pub fn load_orb_yaml(path: String) -> Result<OrbitalConfig, Box<dyn std::error::Error>> {
     let f = std::fs::File::open(path)?;
     let parsed: OrbitalConfig = serde_yaml::from_reader(&f)?;
 
-    println!("{:?}", parsed);
+    // TODO: Place this behind a debug flag
+    //println!("{:?}", parsed);
 
     Ok(parsed)
 }
