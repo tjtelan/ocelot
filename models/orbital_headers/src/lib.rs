5b9ca34351d1dfe61c6c28276796b6534f9d8b1a
T.J. Telan <tjtelan@level11.com>
2019-11-08 14:55:56 -0800

    Replacing tower-grpc with tonic #227 Updating proto style to calm build warnings Workaround for codegen issues #229

--- models/orbital_headers/src/lib.rs
+++ models/orbital_headers/src/lib.rs
@@ -1,24 +1,26 @@
 /// Generated Rust from protobufs for build metadata and build services
 pub mod build_metadata {
-    include!(concat!(env!("OUT_DIR"), "/build_metadata.rs"));
+    tonic::include_proto!("build_metadata");
 }
 
 /// Generated Rust from protobufs for credential handling
+/// Proto compilation issue is tracked by issue https://github.com/level11consulting/orbitalci/issues/229
 pub mod credential {
-    include!(concat!(env!("OUT_DIR"), "/credential.rs"));
+    tonic::include_proto!("credential");
+    tonic::include_proto!("credential_service");
 }
 
 /// Generated Rust from protobufs for external service integration
 pub mod integration {
-    include!(concat!(env!("OUT_DIR"), "/integration.rs"));
+    tonic::include_proto!("integration");
 }
 
 /// Generated Rust from protobufs for high-level units, Organizations
 pub mod organization {
-    include!(concat!(env!("OUT_DIR"), "/organization.rs"));
+    tonic::include_proto!("organization");
 }
 
 /// Generated Rust from protobufs for possible job states
 pub mod state {
-    include!(concat!(env!("OUT_DIR"), "/state.rs"));
+    tonic::include_proto!("state");
 }
